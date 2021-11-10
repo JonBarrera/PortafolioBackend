6fb1ff6ca604f4c73a14c863bd422e5d682d694c
JonBarrera <jonathanibarrera360@gmail.com>
2021-11-09 22:55:25 -0600

    Integracion con Vercel

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 
 //Initialization
 const app = express();
-require('./database');
+require('./src/database');
 
 //Settings
 app.set('port', process.env.PORT || 3000);
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Routes
-app.use('/api/books', require('./routes/books'));
+app.use('/api/books', require('./src/routes/books'));
 
 //Start the serve
 app.listen(app.get('port'), () => {
