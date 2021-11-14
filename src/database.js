303b9dbf160bb14f4a907c331ef8b5ddbe74f9e6
JonBarrera <jonathanibarrera360@gmail.com>
2021-11-14 09:49:07 -0600

    Fixed conection string database

--- src/database.js
+++ src/database.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONGO_URL}`, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useMongoClient:true
+    useUnifiedTopology: true
 })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
